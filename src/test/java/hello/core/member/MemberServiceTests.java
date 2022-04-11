0bbf2eb79d1d21e3afd9cb2eb03c3d25cbf36515
홍채민 <coals0329@cocen.com>
2022-04-11 11:22:13 +0900

    feat(oop): 과관심사의 분리

--- src/test/java/hello/core/member/MemberServiceTests.java
+++ src/test/java/hello/core/member/MemberServiceTests.java
@@ -1,6 +1,8 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTests {
@@ -8,7 +10,16 @@ public class MemberServiceTests {
     // !!!!! 실제 할당 부분이 구현제를 의존 !!!!!
     // !!!!! 구현 객체를 변경하려면 클라이언트 코드를 변경해야 한다 !!!!!
     // DIP 위반, OCP 위반
-    MemberService memberService = new MemberServiceImpl();
+
+    // MemberService memberService = new MemberServiceImpl();
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+         memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
