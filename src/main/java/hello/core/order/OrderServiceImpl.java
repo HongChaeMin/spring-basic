784563c1eb65032e01c152fdd261d049674431b8
홍채민 <coals0329@cocen.com>
2022-04-14 11:20:56 +0900

    feat(autowired): 다양한 의존관계 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,13 +9,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
 
-    @Autowired
+    @Autowired // 생성자가 딱 1개만 있으면 @Autowired 생략해도 자동 주입 된다
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
+    }
+
+    // notion 참고 부탁...
+    // setter 의존 관계 주입
+     @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Autowired (required = false) // 주입할 대상이 없어도 동작하게 함
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Override
