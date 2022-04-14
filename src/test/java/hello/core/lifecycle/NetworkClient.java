49819bfc69ac11620fd7ab8b4dcd8aee76dc9ba1
minHong <57277976+HongChaeMin@users.noreply.github.com>
2022-04-14 16:13:06 +0900

    Feat/bean lifecycle callback (#12)
    * feat(beanLifesysleCallback): 빈 생명주기 콜백 시작
    
    * feat(beanLifecycleCallback): interface InitializingBean, DisposableBean
    
    * feat(beanLifecycleCallback): 빈 등록 초기화, 소멸 메서드
    
    * feat(beanLifecycleCallback): annotation @PostConstruct, @PreDestroy
    
    Co-authored-by: 홍채민 <coals0329@cocen.com>

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,65 @@
+package hello.core.lifecycle;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출 url : " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작 시 호출
+    public void connect() {
+        System.out.println("connect : " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call : " + url + " message : " + message);
+    }
+
+    // 서비스 종료 시 호출
+    public void disconnect() {
+        System.out.println("close : " + url);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("init 호출");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("close 호출");
+        disconnect();
+    }
+
+    // 결과
+    // 생성자 호출 url : null
+    // afterPropertiesSet 호출
+    // connect : http://localhost:80
+    // call : http://localhost:80 message : 초기화 연결 메세지
+    // 15:50:53.519 [main] DEBUG org.springframework.context.annotation.AnnotationConfigApplicationContext - Closing org.springframework.context.annotation.AnnotationConfigApplicationContext@1b11171f, started on Thu Apr 14 15:50:53 KST 2022
+    // destroy 호출
+    // close : http://localhost:80
+    /*@Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet 호출");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroy 호출");
+        disconnect();
+    }*/
+}
